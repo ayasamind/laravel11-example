c4d7aed8d644265fcb68f4eddfc944de111eb910
ayasamind <masayaliketennis@yahoo.co.jp>
2024-05-10 11:35:28 +0900

    環境等を調整

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-});
+export default ({ mode }) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.tsx',
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        server: {
+            host: true,
+            hmr: {
+                host: process.env.VITE_HMR_HOST,
+            },
+        }
+    });
+}
